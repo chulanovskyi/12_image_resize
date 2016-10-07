eb92bfb28c6682c4df06ed38441bf24ca879b4fc
chulanovskyi <skyrocker@mail.ru>
2016-10-07 14:33:27 +0300

    work in progress..

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,41 @@
+from  PIL import Image
+import argparse
+from os.path import basename
 
 
-def resize_image(path_to_original, path_to_result):
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('original_path')
+    parser.add_argument('--width', type=int)
+    parser.add_argument('--height', type=int)
+    parser.add_argument('-s', '--scale', type=float)
+    parser.add_argument('resized_path')
+    return parser
+
+
+def resize_image(**params):
+    image = Image.open(params['original_path'])
+    if params['scale']:
+        width, height = image.size
+        new_size = (int(params['scale']*width), int(params['scale']*height))
+        resized_image = image.resize(new_size)
+    return resized_image
+
+
+def save_image(image, resized_path):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    params = parser.parse_args()
+    if params.scale and params.width or params.scale and params.height:
+        print('Error!')
+        print('If --scale is defined, then --height or --width not allowed')
+        exit(11)
+    new_image = resize_image(**vars(params))
+    if params.resized_path:
+        save_image(new_image, params.resized_path)
+    else:
+        file_name = basename(params.original_path)
+        
