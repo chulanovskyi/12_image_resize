74130c2f5e9be1932c137144e69c78ba2c61b526
chulanovskyi <skyrocker@mail.ru>
2016-10-08 01:50:12 +0300

    resizer 1.0

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # 12_image_resize
+
+###Prerequisites:
+
+Run in console `pip install -r requirements.txt` to install 3rd party modules.
+
+---
+
+Simple command line image resizer. Acceptable format is JPG or PNG.
+Script takes one required and several optional parameters:
+
+- `original_path` - can be short (name of image in same folder) or full (`C:/my_folder/img.jpg`);
+- `--width` - int;
+- `--height` - int;
+- `--scale` - float;
+- `--output` - place where to save image. If not given - will save in the same folder of input image.
+
+'''
+!Note that: if `--scale` is given you cannot add `--height` or `--width`.
+'''
--- image_resize.py
+++ image_resize.py
@@ -8,27 +8,39 @@ def create_parser():
     parser.add_argument('original_path')
     parser.add_argument('--width', type=int)
     parser.add_argument('--height', type=int)
-    parser.add_argument('-s', '--scale', type=float)
-    parser.add_argument('resized_path')
+    parser.add_argument('--scale', type=float)
+    parser.add_argument('--output')
     return parser
 
 
-def resize_image(**params):
-    image = Image.open(params['original_path'])
+def resize_image(image, **params):
+    width, height = image.size
+    image_ratio = round((image.width / image.height), 1)
     if params['scale']:
-        width, height = image.size
         new_size = (int(params['scale']*width), int(params['scale']*height))
-        resized_image = image.resize(new_size)
+    if params['width'] and params['height']:
+        new_size = (params['width'], params['height'])
+    elif params['width']:
+        new_size = (params['width'], int(params['width']/image_ratio))
+    elif params['height']:
+        new_size = (int(params['height']*image_ratio), params['height'])
+    resized_image = image.resize(new_size)
     return resized_image
 
 
-def save_image(image, name, ext, image_path=None):
+def save_image(image, name, ext, out_path=None):
+    width, height = image.size
+    new_file_name = '{name}__{w}x{h}{ext}'.format(
+        name=name,
+        w=width,
+        h=height,
+        ext=ext)
+    if out_path:
+        new_file_name = out_path + new_file_name
     if '.jpg' in ext:
         ext = 'JPEG'
     if '.png' in ext:
         ext = 'PNG'
-    int(width), int(height) = image.size
-    new_file_name = '%s' % image_path
     image.save(new_file_name, ext)
 
 
@@ -39,6 +51,11 @@ if __name__ == '__main__':
         print('Error!')
         print('If [--scale] is defined, then [--height] or [--width] not allowed')
         exit(11)
+    image = Image.open(params.original_path)    
     image_name, image_ext = splitext(basename(params.original_path))
-    new_image = resize_image(**vars(params))
-    save_image(new_image, image_name, image_ext, params.resized_path)
+    new_image = resize_image(image, **vars(params))
+    image_ratio = round((image.width / image.height), 1)
+    new_image_ratio = round((new_image.width / new_image.height), 1)
+    if image_ratio != new_image_ratio:
+        print('Warning! New image size ratio is different than original')
+    save_image(new_image, image_name, image_ext, params.output)
